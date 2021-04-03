f5c9fd51f790ab21da611284d64d0fedec5964cf
Nicholas DiPatri <ndipatri@gmail.com>
2021-04-03 12:40:24 -0400

    Moved over to MQTT

--- particle/workbench/tRacket/src/tRacket.ino
+++ particle/workbench/tRacket/src/tRacket.ino
@@ -1,15 +1,8 @@
 #include <Adafruit_IO_Client.h>
 #include "adafruit-ina219.h"
-#include <TinyGPS++/TinyGPS++.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_SPARK.h"
-
-// GPS
-SYSTEM_THREAD(ENABLED);
-TinyGPSPlus gps;
-
-// We only find one GPS fix during startup and then we shut down the GPS module.
-bool foundAndReportedGPSFix = false;
+#include "DeviceNameHelperRK.h"
 
 // The current meter we will use to determine when battery needs to be
 // recharged.
@@ -38,24 +31,15 @@ Adafruit_MQTT_SPARK mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_
 // These will automatically generate new feeds on the account defined by AIO_KEY
 // https://io.adafruit.com/ndipatri/feeds/coopertownOccupiedT1
 
-// NJD TODO - For now push de-normalized data to one feed so it's easier for client..
-// eventually should push to separate occupancy feed per device
-String occupiedFeedName = "Occupancy"; 
-Adafruit_IO_Feed occupancyFeed = aioClient.getFeed(occupiedFeedName);
-
 String rechargeFeedName = System.deviceID() + "Recharge"; 
 Adafruit_IO_Feed rechargeFeed = aioClient.getFeed(rechargeFeedName);
 
 // We need to use MQTT protocol to transfer lat/lng information to Adafruit
 // The '/csv' postfix is magic that treat the incoming data as a single entry
 // So pushing data with format 'dataValue, lat, long, elevation' will result in a single
-// data point (which is ignored), but it will have lat/long meta data attached.
-String mqttFeedName = AIO_USERNAME + "/feeds/Location/csv"; 
-Adafruit_MQTT_Publish locationMQTTTopic = Adafruit_MQTT_Publish(&mqtt,  mqttFeedName);
-
-
-// Chip enabl for GPS module.
-int GPS_MODULE_ENABLE_OUTPUT_PIN = D6; 
+// data point, but it will have lat/long meta data attached.
+String mqttOccupancyFeedName = AIO_USERNAME + "/feeds/Occupancy"; 
+Adafruit_MQTT_Publish occupancyMQTTTopic = Adafruit_MQTT_Publish(&mqtt,  mqttOccupancyFeedName);
 
 // The sensor is configured NC (normally closed)
 int MOTION_SENSOR_DETECTED_INPUT_PIN = D8; 
@@ -95,28 +79,23 @@ long MOTION_POLL_DURATION_MINUTES = 3;
 
 bool firstPass = true;
 
-// Will remain empty until we get a callback from the cloud shortly
-// after startup
-String deviceName = "";
+// For deviceName retrieval and storage
+SYSTEM_THREAD(ENABLED);
+retained DeviceNameHelperData deviceNameHelperRetained;
+
 
 void setup() {
     
     Serial.begin(115200);
 
-    // The Adafruit GPS module is connected to Serial1 and D6
-    Serial1.begin(9600);
+    // This helps with retrieving Device name
+    DeviceNameHelperRetained::instance().setup(&deviceNameHelperRetained);
+    DeviceNameHelperRetained::instance().withCheckPeriod(24h);
 
-    // Settings D6 LOW powers up the GPS module
-    pinMode(GPS_MODULE_ENABLE_OUTPUT_PIN, OUTPUT);
-    digitalWrite(GPS_MODULE_ENABLE_OUTPUT_PIN, LOW);
 
     // By default, LED on OFF when motion is triggered
     pinMode(MOTION_SENSOR_LED_ENABLE_OUTPUT_PIN, OUTPUT);
     digitalWrite(MOTION_SENSOR_LED_ENABLE_OUTPUT_PIN, LOW);
-
-    // For accelerometer
-	Wire.setSpeed(CLOCK_SPEED_100KHZ);
-	Wire.begin();
 
     powerMeter.begin();
     powerMeter.setCalibration_16V_400mA();
@@ -142,9 +121,7 @@ void setup() {
 }
 
 void loop() {
-    checkForDeviceName();
-
-    checkForAndPublishGPSFix();
+    DeviceNameHelperRetained::instance().loop();
 
     if (PLATFORM_ID != PLATFORM_ARGON && PLATFORM_ID != PLATFORM_BORON) {
         Particle.publish("TERMINAL", "Must be run on Argon or Boron", 60, PUBLIC);
@@ -166,18 +143,14 @@ void loop() {
 
         isMotionDetectedNow = checkForMotion();
 
-        if (isMotionDetectedNow) {
+        if (isMotionDetectedNow && deviceNameFound()) {
             publishParticleLog("activityReport", "motionDetected");
 
             if (!currentlyInMotionPeriod()) {
 
                 // Now starting new motion period
-                if (deviceName.equals("")) {
-                    publishParticleLog("activityReport", "startMotionPeriod(noDeviceNameYet)");
-                } else {
-                    publishParticleLog("activityReport", "startMotionPeriod(" + String(deviceName) + ")");
-                    occupancyFeed.send(String(deviceName) + ":1");
-                }
+                publishParticleLog("activityReport", "startMotionPeriod(" + String(getDeviceName()) + ")");
+                sendOccupancyToCloud(String(getDeviceName()) + ":1");
             }
 
             // Detecting new motion extends our current motion period
@@ -197,12 +170,8 @@ void loop() {
         // external monitoring    
         resetMotion();
 
-        if (deviceName.equals("")) {
-            publishParticleLog("activityReport", "endMotionPeriod(noDeviceNameYet");
-        } else {
-            publishParticleLog("activityReport", "endMotionPeriod(" + String(deviceName) + ")");
-            occupancyFeed.send(String(deviceName) + ":0");
-        }
+        publishParticleLog("activityReport", "endMotionPeriod(" + String(getDeviceName()) + ")");
+        sendOccupancyToCloud(String(getDeviceName()) + ":0");
     }
 
     checkBattery(firstPass);
@@ -233,16 +202,12 @@ void loop() {
     }
 }
 
-void checkForDeviceName() {
-    if (deviceName == "") {
-        Particle.subscribe("particle/device/name", nameHandler);
-        Particle.publish("particle/device/name");
-    }
+bool deviceNameFound() {
+    return DeviceNameHelperRetained::instance().hasName();
 }
 
-void nameHandler(const char *topic, const char *data) {
-    publishParticleLog("activityReport", "deviceNameReceived:" + String(data));
-    deviceName = String(data);    
+char const* getDeviceName() {
+    return DeviceNameHelperRetained::instance().getName();
 }
 
 void expireStartupTestModeIfNecessary() {
@@ -256,56 +221,38 @@ void expireStartupTestModeIfNecessary() {
     }
 }
 
-void checkForAndPublishGPSFix() {
-    if (!foundAndReportedGPSFix) {
+void sendOccupancyToCloud(const char *occupancyMessage) {
 
-        // check for available data from GPS module
-        while (Serial1.available() > 0) {
-            if (gps.encode(Serial1.read())) {
+    MQTTConnect();
 
-                // Valid data received from GPS module ...    
-                if (gps.location.isValid()) {
+    occupancyMQTTTopic.publish(String(occupancyMessage) + "," + String(0) + "," + String(0) + ",10");
 
-                    Particle.publish("gpsUpdate", "lat:" + String(gps.location.lat()) + ", lon:" + String(gps.location.lng()), 60, PUBLIC);
-                    foundAndReportedGPSFix = true;
+    publishParticleLog("mqtt", "Message sent (" + String(occupancyMessage) + ").");
 
-                    sendLatLongToCloud(gps.location.lat(), gps.location.lng());
+    MQTTDisconnect();
+}
 
-                    // Turn off GPS module
-                    digitalWrite(GPS_MODULE_ENABLE_OUTPUT_PIN, HIGH);
-                }
-            }
-        }
+void MQTTConnect() {
+    int8_t ret;
+
+    // Stop if already connected.
+    if (mqtt.connected()) {
+        return;
     }
+
+    publishParticleLog("mqtt", "Connecting to MQTT server...");
+
+    while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+        publishParticleLog("mqtt", "Retrying MQTT connect from error: " + String(mqtt.connectErrorString(ret)));
+        mqtt.disconnect();
+        delay(5000);  // wait 5 seconds
+    }
+    publishParticleLog("mqtt", "MQTT Connected!");
 }
 
-void sendLatLongToCloud(double lat, double lng) {
-
-    Particle.publish("gpsUpdate", "Connecting to MQTT Server", 60, PUBLIC);
-    MQTTconnect();
-
-    String deviceIdString = System.deviceID();
-    locationMQTTTopic.publish(deviceIdString + "," + String(lat) + "," + String(lng) + ",10");
-
+void MQTTDisconnect() {
     mqtt.disconnect();
-}
-
-void MQTTconnect() {
-  int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    return;
-  }
-
-  Particle.publish("gpsUpdate", "Connecting to Adafruit IO MQTT Server", 60, PUBLIC);
-
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Particle.publish("gpsUpdate", "Retrying MQTT connect from error: " + String(mqtt.connectErrorString(ret)), 60, PUBLIC);
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-  }
-  Particle.publish("gpsUpdate", "MQTT Connected!", 60, PUBLIC);
+    publishParticleLog("mqtt", "MQTT Connected!");
 }
 
 bool checkForMotion() {
